cd153802cc69fa7a3398f0759230370cd5b9fb8d
aniWeyn <4niweyn@gmail.com>
2023-01-13 13:17:19 +0100

    updated slide

--- src/components/shapes/slide01.vue
+++ src/components/shapes/slide01.vue
@@ -2,7 +2,12 @@
   <div class="slide">
     <hr />
     <h3>CSS Shapes</h3>
-    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ihL8vd__AMA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe height="600" style="width: 100%;" scrolling="no" title="CSS Cupcakes" src="https://codepen.io/poulamic/embed/mdPPqbx?default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href="https://codepen.io/poulamic/pen/mdPPqbx">
+  CSS Cupcakes</a> by Poulami Chakraborty (<a href="https://codepen.io/poulamic">@poulamic</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+</iframe>
+    <iframe width="560" height="615" src="https://www.youtube-nocookie.com/embed/ihL8vd__AMA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <ul>
       <li>Open CodePen</li>
       <li>Try to solve exercises</li>
