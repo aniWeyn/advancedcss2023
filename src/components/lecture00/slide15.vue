df3d7aaa4aa6e051bd373c4e24220e3894b8c7d9
aniWeyn <4niweyn@gmail.com>
2023-01-23 14:21:31 +0100

    added highlights

--- src/components/lecture00/slide15.vue
+++ src/components/lecture00/slide15.vue
@@ -4,11 +4,11 @@
     <h3>Groups</h3>
     <p>This week you will work with someone else everyday.</p>
     <p>
-      On Friday Blackboard will assign you randomly to groups. Groups has usually 3 members, but can be as well 2 members.
+      On Friday Blackboard will assign you randomly to groups. <span class="action--underscore">Groups has usually 3 members</span>, but can be as well 2 members.
     </p>
     <p>
-      You are assign to groups randomly to help you practice work with anyone,
-      as well to bond with your colleagoues with which you maybe didn't have a
+      You are assign to groups <span class="action--underscore">randomly</span> to <strong>help you practice work with anyone</strong>,
+      as well <strong>to bond with your colleagoues</strong> with which you maybe didn't have a
       chance to talk to yet. Thanks to that I hope you will be better integrated
       within you bachelor program.
     </p>
