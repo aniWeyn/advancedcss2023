9d4a6a418246ad249368d3cb51d27fe9019f36c8
aniWeyn <4niweyn@gmail.com>
2023-02-23 13:06:09 +0100

    added reasons for codepen

--- src/views/laboratories/LabCodePen.vue
+++ src/views/laboratories/LabCodePen.vue
@@ -4,6 +4,10 @@
     <p class="space">
       <a target="_blank" href="https://codepen.io/">Code Pen</a>
     </p>
+    <ul>
+      <li>no enviroment to configure</li>
+      <li>the code is immidietely rendered in preview window</li>
+    </ul>
     <p>
       The best place to build, test, and discover front-end code.
     </p>
@@ -11,30 +15,33 @@
       CodePen is a social development environment for front-end designers and
       developers.
     </p>
-    <p>Build and deploy a website, show off your work, build test
-      cases to learn and debug, and find inspiration.</p>
-        <img
-          class="img"
-          src="../../assets/images/CodePen.png"
-          alt="CodePen"
-        />
+    <p>
+      Build and deploy a website, show off your work, build test cases to learn
+      and debug, and find inspiration.
+    </p>
+    <img class="img" src="../../assets/images/CodePen.png" alt="CodePen" />
     <h4>How to use codepen without registation?</h4>
     <p>Click in left corner "Start coding"</p>
-    <p>Disadvantages: you will lose your work, you will not be able to share your work</p>
-<h4>Exercises</h4>
-<h5>Create CodePen account</h5>
-<p>Create CodePen account and explore its' features and test the code editor.</p>
-<h4>Set up for SASS</h4>
-        <img
-          class="img"
-          src="../../assets/images/codepen_setup_for_scss.png"
-          alt="CodePen"
-        />
+    <p>
+      Disadvantages: you will lose your work, you will not be able to share your
+      work
+    </p>
+    <h4>Exercises</h4>
+    <h5>Create CodePen account</h5>
+    <p>
+      Create CodePen account and explore its' features and test the code editor.
+    </p>
+    <h4>Set up for SASS</h4>
+    <img
+      class="img"
+      src="../../assets/images/codepen_setup_for_scss.png"
+      alt="CodePen"
+    />
   </div>
 </template>
 
 <style scoped>
-.img{
-    width: 70vw;
+.img {
+  width: 70vw;
 }
 </style>
